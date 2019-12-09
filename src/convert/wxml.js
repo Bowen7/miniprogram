45939730fc02c8ea52edb39fb3924eb13a9c4b08
张文翔 <771797746@qq.com>
2019-12-10 00:47:58 +0800

    代码格式

--- src/convert/wxml.js
+++ src/convert/wxml.js
@@ -4,22 +4,22 @@ module.exports = function(source) {
   const jsContent = h.convertFile(resourcePath, 'script');
   // console.log(h.convertJs(jsContent));
   return `<template>
-	<div>123</div>
-</template>
+    <div>123</div>
+  </template>
 
-<script>
-export default Page({
-	name: "Test",
-	components: {},
-	data: function() {
-		return {};
-	},
-	mounted() {
-		console.log(this.$options._scopeId, this._uid);
-	}
-});
-</script>
+  <script>
+  export default Page({
+    name: "Test",
+    components: {},
+    data: function() {
+      return {};
+    },
+    mounted() {
+      console.log(this.$options._scopeId, this._uid);
+    }
+  });
+  </script>
 
-<style scoped></style>
-`;
+  <style scoped></style>
+  `;
 };
